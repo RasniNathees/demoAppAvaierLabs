ffb35672b4a76e300cb460dad0c59aa3d56fd506
rasni <rasninathees.se@gmail.com>
2025-09-10 12:21:16 +0530

    changes in layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,23 +5,15 @@ interface  LayoutProps {
     children: ReactNode
 }
 
-const Layout = ({ children }: LayoutProps) => {
-    return (
-        <div className="admin-layout">
-            {/* <div className="lg:hidden">mobile topbar</div> */}
-            <div className="w-screen block">
-                <TopBar />
-            </div>
-            <div className="flex flex-col bg-[var(--light)] lg:w-screen">
-                
-                <aside className="children">
-                    <div className="mx-4">
-                        {children}
-                    </div>
-                </aside>
-            </div>
-        </div>
-    )
-}
+const Layout = ({ children}:LayoutProps ) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <TopBar />
+      <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
