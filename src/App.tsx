7c775d9e022f457c84d5414cb81d0b5ddd2a569b
rasni <rasninathees.se@gmail.com>
2025-09-10 15:39:32 +0530

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Dashboard} from "./components/index"
-
+import { Dashboard } from "./components/index"
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
-    
-    <Dashboard/>
+    <>
+      <Helmet>
+        <title>DemoApp</title>
+        <meta name="description" content="A broker dashboard application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
+      <Dashboard />
+    </>
   )
 }
- 
+
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppProvider } from './appcontext';
+import { HelmetProvider } from "react-helmet-async";
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <HelmetProvider>
+         <App />
+      </HelmetProvider>
     </AppProvider>
 
   </StrictMode>,
